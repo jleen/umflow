893444ffd7d9eab6e5627fcabe480feb78181f82
John Leen <jleen@saturnvalley.org>
2024-01-04 19:57:11 -0500

    Boxes within boxes

--- src/Main.elm
+++ src/Main.elm
@@ -40,11 +40,8 @@ update msg (Model model _) =
 
 newRow = [T, T, T, T, T]
 
-
-
 saurImg = "https://images.squarespace-cdn.com/content/v1/"
           ++ "5400890ee4b03f524b003725/1415037601583-POTPRXXO72EZZ87SWRTU/favicon.ico?format=100w"
-
 
 greenBox =
     rect [ x "100", y "10", width "40", height "40", fill "green"
@@ -55,6 +52,17 @@ bagelSpin rot =
     foreignObject [ x "10", y "100", width "100", height "100", rotation rot ]
                   [ img [src saurImg] [] ]
 
+pipebox : Int -> Int -> Int -> Int -> Svg msg
+pipebox xx yy ww hh =
+    svg [ x <| String.fromInt xx, y <| String.fromInt yy
+        , width <| String.fromInt ww, height <| String.fromInt hh
+        , viewBox "0 0 10 10"
+        ]
+        [ rect [ x "1", y "1", height "8", width "8", fill "red" ] []
+        , rect [ x "2", y "2", height "6", width "6", fill "blue" ] []
+        ]
+
+
 view : Model -> Html Msg
 view (Model m theta) =
   let rot = String.fromInt theta in
@@ -63,7 +71,7 @@ view (Model m theta) =
     , width "400"
     , height "400"
     ]
-    [ greenBox, bagelSpin rot ]]
+    [ greenBox, bagelSpin rot, pipebox 10 10 80 80 ]]
 
 rotation rot =
     transform ("rotate(" ++ rot ++ ", 60, 150)")
